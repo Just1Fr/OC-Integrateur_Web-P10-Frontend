e80232a288b9a782d5a44b69d57a5b0894e4ce7d
Just1Fr <190003579+Just1Fr@users.noreply.github.com>
2026-04-20 17:59:24 +0200

    refactor: convert Home feature cards into a reusable component

---
+++ ArgentBank-Frontend/src/components/FeatureCard/index.jsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+export default function FeatureCard({ title="", description="", imgSrc="", imgAlt="" }) {
+    return (
+        <div className="feature-item">
+            <img src={imgSrc} alt={imgAlt} className="feature-icon" />
+            <h3 className="feature-item-title">{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
